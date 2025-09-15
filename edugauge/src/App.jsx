2444aff5e59e45757a9bdcc3b3dee01d3849d5d1
BooiBotle <botlebooi10@gmail.com>
2025-09-15 19:35:55 +0200

    Edited the SignUpPage and as well as the dashboard

--- edugauge/src/App.jsx
+++ edugauge/src/App.jsx
@@ -1,23 +1,26 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/login.jsx";
 import Dashboard from "./pages/dashboard.jsx";
-import SignUp from  "./pages/SignUp.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import eFundiTest from "./pages/eFundiTest.jsx";
 import "./App.css";
+
 
 function App() {
   return (
     <Router>
       <nav style={{ padding: "10px", background: "#333" }}>
-        <Link to="/" style={{ margin: "0 10px", color: "#fff" }}>Home</Link>
-        <Link to="/login" style={{ margin: "0 10px", color: "#fff" }}>Login</Link>
-        <Link to="/dashboard" style={{ margin: "0 10px", color: "#fff" }}>Dashboard</Link>
+        <Link to="/" style={{ margin: "0 10px", color: "#270303ff" }}>Home</Link>
+        <Link to="/login" style={{ margin: "0 10px", color: "#4e074bff" }}>Login</Link>
+        <Link to="/dashboard" style={{ margin: "0 10px", color: "#07096bff" }}>Dashboard</Link>
+        <Link to="/SignUpPage" style={{ margin: "0 10px", color: "#fff" }}>SignUpPage</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/pages/SignUp" element={<SignUp />} />
+        <Route path="/SignUpPage" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
