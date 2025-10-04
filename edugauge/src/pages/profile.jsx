dcd1f0f34c46ec1ba1c952fcb9d1b39a4e7acc54
Erika <erikahaasbroek86@gmail.com>
2025-10-04 18:21:01 +0200

    Resoved all problems

--- edugauge/src/pages/profile.jsx
+++ edugauge/src/pages/profile.jsx
@@ -66,7 +66,6 @@ const ProfilePage = () => {
     return (
         <div className="page-container">
             {/* Sidebar */}
-<<<<<<< HEAD
             <aside className="sidebar">
                 <div className="logo-section">
                     <img src="/Eduguage-logo.jpg" alt="Eduguage Logo" className="logo-img" />
@@ -82,11 +81,6 @@ const ProfilePage = () => {
                     </ul>
                 </nav>
             </aside>
-=======
-           
-
-            {/* Main Content Area */}
->>>>>>> origin/FRONT-END
             <main className="main-content">
                 <header className="main-header">
                     <Link to="/profile" className="profile-link">
@@ -103,15 +97,9 @@ const ProfilePage = () => {
                         <div className="profile-icon-large">
                             👤
                         </div>
-<<<<<<< HEAD
-                        <div className="profile-name">{displayName}</div> {/*Display actual name instead of a placeholder*/}
-                        <div className="student-number">{displayStudentNumber}</div>
-                        <button className="edit-button">Edit Profile</button>
-=======
                         <div className="profile-name">{placeholderStudentData.name}</div>
                         <div className="student-number">{placeholderStudentData.studentNumber}</div>
                        {/* <button className="edit-button">Edit Profile</button>*/}
->>>>>>> origin/FRONT-END
                     </div>
                     {/* My Information */}
                     <div className="data-box info-box-1">
